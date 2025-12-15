f9e1e9be203880ff5ac61fcaa474bab53e7984cc
HASAN HUSEYIN OZTUNC <hasan.oztunc@atmosware.turkcell.com.tr>
2025-12-15 22:27:39 +0300

    Initial commit

---
+++ src/main/java/com/hasanoztunc/library_automation/LibraryAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.hasanoztunc.library_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasanoztunc/library_automation/LibraryAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasanoztunc.library_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

88954f3d543dfc69c92b1896bc9bf0ecc6048c8b
HASAN HUSEYIN OZTUNC <hasan.oztunc@atmosware.turkcell.com.tr>
2025-12-20 17:46:30 +0300

    Create authors

---
+++ src/main/java/com/hasanoztunc/library_automation/features/author/AuthorController.java
@@ -0,0 +1,60 @@
+package com.hasanoztunc.library_automation.features.author;
+
+import com.hasanoztunc.library_automation.common.payload.GenericResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<GenericResponse<AuthorDTO>> createAuthor(@RequestBody AuthorDTO request) {
+        var response = authorService.createAuthor(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<GenericResponse<List<AuthorDTO>>> getAllAuthors() {
+        var response = authorService.getAllAuthors();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenericResponse<AuthorDTO>> getAuthorById(@PathVariable Long id) {
+        var response = authorService.getAuthorById(id);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<GenericResponse<List<AuthorDTO>>> searchAuthorsByName(@RequestParam String name) {
+        var response = authorService.searchAuthorsByName(name);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GenericResponse<AuthorDTO>> updateAuthor(@PathVariable Long id, @RequestBody AuthorDTO request) {
+        var response = authorService.updateAuthor(id, request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GenericResponse<Void>> deleteAuthor(@PathVariable Long id) {
+        var response = authorService.deleteAuthor(id);
+
+        return ResponseEntity.ok(response);
+    }
+}
